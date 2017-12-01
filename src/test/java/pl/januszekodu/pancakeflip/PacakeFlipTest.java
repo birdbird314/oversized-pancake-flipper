7ccdd5d8c8c6c5e22ad678e4c186704afeca1974
birdbird314 <grantstadt@gmail.com>
2017-12-01 19:16:45 +0100

    Initial commit

---
+++ src/test/java/pl/januszekodu/pancakeflip/PacakeFlipTest.java
@@ -0,0 +1,12 @@
+package pl.januszekodu.pancakeflip;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PacakeFlipTest {
+  @Test
+  public void shouldRun() {
+    assertThat("hello").isEqualTo("hello");
+  }
+}
